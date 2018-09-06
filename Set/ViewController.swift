0020602366ccfa082975cf9b11685773f576836d
Arturo_Kuang <arturo.kuang@uabc.edu.mx>
2018-09-06 09:48:39 -0700

    Initial commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by comp05A on 9/6/18.
+//  Copyright © 2018 Universidad Autonoma de Baja California. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
